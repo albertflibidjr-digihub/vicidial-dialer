7faa3348baae0876d744270b4b88944acbb32d91
albertflibidjr-digihub <albertflibidjr@gmail.com>
2026-02-04 15:25:52 -0800

    Update app.js

--- app.js
+++ app.js
@@ -589,7 +589,7 @@ async function setDisposition(code) {
         showNotification(`Disposition set: ${code}`, 'success');
 
         setTimeout(() => {
-            pauseAgent();
+            pauseAgentApi();
         }, 500);
     } else {
         showNotification('Failed to set disposition: ' + result.message, 'error');
